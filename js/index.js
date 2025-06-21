048e6f15eb6a6fcd00965dba84f3fb77d6ee5d40
Patala <santiagopatala@gmail.com>
2025-06-21 17:42:56 -0300

    Update

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("Index");
+
+function validarLogin(){
+const formularioLogin = document.getElementById("formularioLogin");
+const botonLogin = document.getElementById("botonLogin");
+
+const nombreDeUsuario = formularioLogin.querySelector("#usuario");
+
+const contraseña = formularioLogin.querySelector("#contraseña");
+
+}
+
+validarLogin();
---
+++ js/recuperacion.js
@@ -0,0 +1,1 @@
+console.log("Recuperacion de contraseña");
