60dace50e8e6d8f23dcd7acf53064458be4fb04e
Patala <santiagopatala@gmail.com>
2025-07-01 15:45:22 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
 console.log("Index");
-
-function validarLogin(){
-const formularioLogin = document.getElementById("formularioLogin");
-const botonLogin = document.getElementById("botonLogin");
-
-const nombreDeUsuario = formularioLogin.querySelector("#usuario");
-
-const contraseña = formularioLogin.querySelector("#contraseña");
-
-}
-
-validarLogin();
